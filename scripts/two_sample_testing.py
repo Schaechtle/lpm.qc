5f2f9d57c5358406167ae08edd79a5551b3d52be
Schaechtle <u.schaechtle@gmail.com>
2024-02-18 13:00:01 -0500

    WIP: make chi-squared more robust

--- scripts/two_sample_testing.py
+++ scripts/two_sample_testing.py
@@ -13,17 +13,19 @@ def chi_squared(vector1_in, vector2_in):
     # Create a contingency table
     vector1 = only_strings(vector1_in)
     vector2 = only_strings(vector2_in)
+    if len(vector1) <  len(vector2):
+        vector2 = vector2[0:len(vector1)]
     unique_strings = list(set(vector1 + vector2))
     contingency_table = np.zeros((len(unique_strings), 2))
-
     for i, string in enumerate(unique_strings):
         contingency_table[i, 0] = vector1.count(string)
         contingency_table[i, 1] = vector2.count(string)
 
     # Perform the chi-squared test
-    chi2, p, dof, expected = chi2_contingency(contingency_table)
-    # For now, only return p.
-    return p, chi2
+    chi2, p, dof, expected = chi2_contingency(contingency_table, correction=True)
+    # Apply heuristic about when Chi-squared is not supposed to work.
+    problem = (np.min(contingency_table[:,0]) == 1.) and (dof <= 5)
+    return p, chi2, problem
 
 
 def main():
@@ -50,13 +52,15 @@ def main():
 
     results = []
     num_tests = df_compare.shape[1]
+    num_baselines = 3 # XXX: this should not be hard-coded.
     for c in df_training.columns:
-        p, chi2 = chi_squared(df_training[c].tolist(), df_compare[c].tolist())
+        p, chi2, problem = chi_squared(df_training[c].tolist(), df_compare[c].tolist())
         result = {
             "column":c,
             "p":p,
             "test_statistic":chi2,
-            "significant_after_correction": (p < (0.01/(num_tests*3)))
+            "significant_after_correction": (p < (0.01/(num_tests*num_baselines))),
+            "problem_with_test": problem
         }
         results.append(result)
     pd.DataFrame(results).to_csv(args.output, index=False)
